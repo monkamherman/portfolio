93992c370e2b89174130290feec757860fc3788b
monkamherman <cesaristos5@gmail.com>
2025-06-03 22:45:28 +0100

    fix: remove unused imports and add noreferrer to external links in Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {FaGithub, FaLinkedin} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
 import {GrMail} from "react-icons/gr";
-import {SiLeetcode} from "react-icons/si"
 
 const Footer = () => {
   return (
@@ -9,8 +8,8 @@ const Footer = () => {
       <h4>Developed by MH2pro</h4>
       <h4>Copyright &copy; 2025 MH2pro</h4>
       <div className='footerLinks'>
-        <a href="https://github.com/DevanshSahni" target='_blank'><FaGithub/></a>
-        <a href='mailTo:herman.moukam5@gmail.com' target='_blank'><GrMail/></a>
+        <a href="https://wa.me/237658852731" target='_blank' rel='noreferrer'><FaGithub/></a>
+        <a href='mailTo:herman.moukam5@gmail.com' target='_blank' rel='noreferrer'><GrMail/></a>
        
       </div>
     </footer>
